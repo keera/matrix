bf6851d83e344d49c5276cb82dfec424cd5ed2b3
Alan <linpd2@gmail.com>
2014-03-23 20:21:28 -0400

    [minor] styles

--- app/scripts/views/header.js
+++ app/scripts/views/header.js
@@ -13,12 +13,26 @@ define(function(require) {
 
     events: {
       "click .new-file": "newFile",
-      "click .new-label": "newLabel"
+      "click .new-label": "newLabel",
+      "click .about": "updateAbout"
     },
 
     newFile: function() {
       // Create new file on server
       window.open("#file/5/edit", "_blank");
+    },
+
+    updateNav: function(current) {
+      $('.navbar-nav li')
+        .removeClass('active')
+        .filter(function() {
+          return $(this).children().attr('class') == current;
+        })
+        .addClass('active');
+    },
+
+    updateAbout: function() {
+      this.updateNav("about");
     },
 
     newLabel: function() {
