d161e326ae5448e926ea62f68a4db1043c46263b
Alan <linpd2@gmail.com>
2014-05-17 19:24:49 -0400

    updated jshint and fix indent in main

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,52 +2,51 @@
 "use strict";
 
 require.config({
-    shim: {
-        underscore: {
-            exports: "_"
-        },
-        backbone: {
-            deps: [
-                "underscore",
-                "jquery"
-            ],
-            exports: "Backbone"
-        },
-        handlebars: {
-          exports: "Handlebars"
-        },
-        markdown: {
-          exports: "markdown"
-        },
-        bootstrap: {
-          deps: ["jquery"]
-        },
-        select2: {
-          deps: ["jquery"],
-          exports: "Select2"
-        },
+  shim: {
+    underscore: {
+      exports: "_"
     },
-    paths: {
-        jquery: "../bower_components/jquery/jquery",
-        backbone: "../bower_components/backbone/backbone",
-        underscore: "../bower_components/underscore/underscore",
-        markdown: "../bower_components/markdown/lib/markdown",
-        mousetrap: "../bower_components/mousetrap/mousetrap",
-        text: "../bower_components/requirejs-text/text",
-        handlebars: "../bower_components/handlebars/handlebars",
-        bootstrap: "../bower_components/bootstrap/dist/js/bootstrap",
-        select2: "../bower_components/select2/select2",
-        moment: "../bower_components/momentjs/moment"
-    }
+    backbone: {
+      deps: [
+          "underscore",
+          "jquery"
+      ],
+      exports: "Backbone"
+    },
+    handlebars: {
+      exports: "Handlebars"
+    },
+    markdown: {
+      exports: "markdown"
+    },
+    bootstrap: {
+      deps: ["jquery"]
+    },
+    select2: {
+      deps: ["jquery"],
+      exports: "Select2"
+    },
+  },
+  paths: {
+    jquery: "../bower_components/jquery/jquery",
+    backbone: "../bower_components/backbone/backbone",
+    underscore: "../bower_components/underscore/underscore",
+    markdown: "../bower_components/markdown/lib/markdown",
+    mousetrap: "../bower_components/mousetrap/mousetrap",
+    text: "../bower_components/requirejs-text/text",
+    handlebars: "../bower_components/handlebars/handlebars",
+    bootstrap: "../bower_components/bootstrap/dist/js/bootstrap",
+    select2: "../bower_components/select2/select2",
+    moment: "../bower_components/momentjs/moment"
+  }
 });
 
 require([
-    "backbone",
-    "routers/router",
-    "bootstrap",
+  "backbone",
+  "routers/router",
+  "bootstrap",
 ], function (Backbone, router, bootstrap) {
-    new router();
-    Backbone.history.start();
-    console.log("Hello from Backbone!");
-
+  new router();
+  Backbone.history.start();
 });
+
